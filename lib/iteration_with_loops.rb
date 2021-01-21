c3bc7186275a3c984eba18f39aaf5ad175d14508
Yu Shi <yushiys95@gmail.com>
2021-01-21 04:25:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -17,6 +17,19 @@ p array_of_arrays[2][1]
  # => 8
  if array_of_arrays[0][0].even?
  p array_of_arrays[0][0]
+ def find_even_values(src)
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
+end
 end
 if array_of_arrays[0][1].even?
  p array_of_arrays[0][1]
